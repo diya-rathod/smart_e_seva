a1d05c34979e321aebe0379be5bc9760545a1948
Chirag Solanki <chiragsolanki27012004@gmail.com>
2025-11-23 21:16:16 +0530

    Change Security

--- backend/src/main/java/com/smarteseva/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/smarteseva/backend/config/SecurityConfig.java
@@ -141,8 +141,8 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        // configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://smart-e-seva.vercel.app"));
-        configuration.setAllowedOriginPatterns(Arrays.asList("*"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://smart-e-seva.vercel.app"));
+        // configuration.setAllowedOriginPatterns(Arrays.asList("*"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
