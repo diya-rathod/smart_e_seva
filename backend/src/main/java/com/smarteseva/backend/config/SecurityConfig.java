4bef1428f97c85da096e32d57dcce8782ab28af1
Chirag Solanki <chiragsolanki27012004@gmail.com>
2025-11-23 15:33:37 +0530

    Allow Access to Deployed Link

--- backend/src/main/java/com/smarteseva/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/smarteseva/backend/config/SecurityConfig.java
@@ -141,7 +141,7 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://smart-e-seva.vercel.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
